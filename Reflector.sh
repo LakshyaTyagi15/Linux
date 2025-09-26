70a4183a141f33b42368b462971aef2a64948a76
Lakshya Tyagi <86817577+LakshyaTyagi15@users.noreply.github.com>
2025-09-26 21:11:32 +0530

    Update Reflector.sh

--- Reflector.sh
+++ Reflector.sh
@@ -5,3 +5,9 @@ sudo pacman -S reflector
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
 sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Use Mirror closer to India
+sudo reflector --country IN --country SG --country DE --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Increase time limit of Mirror
+sudo reflector --verbose --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist --download-timeout 15
